26401dc7e7064090f7e57723e1a77653d40dd4b8
Adam Stegman <astegman@gmail.com>
2013-12-29 16:48:07 -0800

    New Rails application named PhotoAlbums with -d postgresql -T

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PhotoAlbums::Application.config.secret_key_base = 'e47c1391abc1e0fc3a97ed340c31b00dbf82e616b5448d037f0856de85b1bc9a524370aa4b40ad88b23e159b8018fb9cc3cbbea101b940ff4f184d2af9f95a11'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PhotoAlbums::Application.config.session_store :cookie_store, key: '_photo_albums_session'
