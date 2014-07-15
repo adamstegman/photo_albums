96ffc31f411726e87bbe4e66a55779b81e53f59b
Adam Stegman <astegman@gmail.com>
2014-07-14 22:49:29 -0700

    Move upload event to the upload route so it can transition

--- spec/javascripts/routes/album_upload_route_spec.js
+++ spec/javascripts/routes/album_upload_route_spec.js
@@ -8,16 +8,17 @@ describe('PhotoAlbums.AlbumUploadRoute', function() {
     route = testHelper.lookup('route', 'album.upload');
   });
 
+  var controller;
+  beforeEach(function() {
+    controller = testHelper.lookup('controller', 'album.upload');
+    controllerHelper.setModel(controller, 'album');
+    route.setupController(controller, undefined);
+  });
+
   describe('#setupController', function() {
     var subject = function() {
       route.setupController(controller, undefined);
     };
-
-    var controller;
-    beforeEach(function() {
-      controller = testHelper.lookup('controller', 'album.upload');
-      controllerHelper.setModel(controller, 'album');
-    });
 
     describe('sets route properties:', function() {
       describe('title', function() {
@@ -56,6 +57,108 @@ describe('PhotoAlbums.AlbumUploadRoute', function() {
         it("is the album name", function() {
           subject();
           expect(applicationController.get('parentHeader')).toBe("Inbox");
+        });
+      });
+    });
+  });
+
+  describe('upload action', function() {
+    it("uploads the photo to S3 using a blob session from the server", function() {
+      var params, callback;
+      var putObject = jasmine.createSpy('putObject');
+      putObject.and.callFake(function(p, c) {
+        params = p;
+        callback = c;
+      });
+      var blobSession = jasmine.createSpyObj('blobSession', ['get']);
+      blobSession.get.and.callFake(function(param) {
+        if (param === 'id') {
+          return 'some-bucket';
+        } else if (param === 's3') {
+          return {putObject: putObject};
+        }
+      });
+      var blobSessionPromise = new Ember.RSVP.Promise(function(resolve) {
+        Ember.run(function() {
+          resolve(blobSession);
+        });
+      });
+      spyOn(route, 'getSession').and.returnValue(blobSessionPromise);
+      spyOn(uuid, 'v4').and.returnValue("some-key");
+
+      route._upload("some-data", {ContentType: "some/type"});
+
+      expect(params.Bucket).toBe("some-bucket");
+      expect(params.Key).toBe("some-key");
+      expect(params.Body).toBe("some-data");
+      expect(params.ContentType).toBe("some/type");
+      expect(typeof callback).toBe("function");
+    });
+
+    it("uploads the photo information to the server", function() {
+      // FIXME
+    });
+  });
+
+  describe('getSession', function() {
+    beforeEach(function() {
+      PhotoAlbums.ApplicationAdapter = DS.ActiveModelAdapter.extend();
+    });
+    afterEach(function() {
+      PhotoAlbums.ApplicationAdapter = DS.FixtureAdapter.extend();
+    });
+
+    afterEach(function() {
+      route.store.all('blobSession').forEach(function(session) {
+        session.deleteRecord();
+        route.store.dematerializeRecord(session);
+      });
+    });
+
+    beforeEach(function() {
+      jasmine.Ajax.install();
+    });
+    afterEach(function() {
+      jasmine.Ajax.uninstall();
+    });
+    var stubSuccessfulBlobSessionRequest = function(id) {
+      jasmine.Ajax.stubRequest('/blob_session').andReturn({
+        status: 200,
+        contentType: 'application/json',
+        responseText: '{"blob_session":{"id":"' + id + '","access_key_id":"some-access-key"}}'
+      });
+    };
+
+    it("returns a promise for session credentials", function(done) {
+      stubSuccessfulBlobSessionRequest('session-bucket-promise');
+      route.getSession().then(function(blobSession) {
+        expect(blobSession.get('id')).toBe('session-bucket-promise');
+        expect(blobSession.get('accessKeyId')).toBe("some-access-key");
+        done();
+      });
+    });
+
+    describe("when a session for the same bucket already exists", function() {
+      beforeEach(function() {
+        Ember.run(function() {
+          route.store.push('blobSession', {id: "existing-bucket", accessKeyId: "should-be-gone"});
+        });
+      });
+
+      it("creates a new session with the same id", function(done) {
+        stubSuccessfulBlobSessionRequest('existing-bucket');
+
+        route.getSession().then(function(blobSession) {
+          expect(blobSession.get('id')).toBe('existing-bucket');
+          expect(blobSession.get('accessKeyId')).toBe("some-access-key");
+
+          var size = 0;
+          route.store.all('blobSession').forEach(function(session) {
+            size++;
+            expect(session).toBe(blobSession);
+            expect(size).toBe(1);
+          });
+          done();
         });
       });
     });
