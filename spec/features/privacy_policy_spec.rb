f7038abf78cce119df6a2a859ed8b3a059deaf11
Adam Stegman <astegman@gmail.com>
2015-02-06 13:24:08 -0800

    Pend privacy policy test until it is added

--- spec/features/privacy_policy_spec.rb
+++ spec/features/privacy_policy_spec.rb
@@ -2,8 +2,11 @@ require 'feature_spec_helper'
 
 describe "Privacy Policy" do
   it "is displayed" do
+    privacy_policy_path = File.expand_path('../../../app/views/static_pages/privacy.html', __FILE__)
+    pending "Cannot test this without a privacy policy" unless File.exists?(privacy_policy_path)
+
     visit '/privacy'
-    expected_content = File.read(File.expand_path('../../../app/views/static_pages/privacy.html', __FILE__))
+    expected_content = File.read(privacy_policy_path)
     expect(Nokogiri::HTML(page.html).to_s.squeeze("\n")).to eq(Nokogiri::HTML(expected_content).to_s.squeeze("\n"))
   end
 end
